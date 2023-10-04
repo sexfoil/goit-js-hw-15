a6e2a94612e9fbb2e37a616b1b864dafab23dbc9
Slava Poliakov <78slavapolyakov@gmail.com>
2023-10-04 17:28:26 +0300

    Fixed Logic

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -2,8 +2,15 @@ const inputElement = document.querySelector("#validation-input");
 
 const checkInputLength = () => {
     const dataLength = Number(inputElement.attributes.getNamedItem("data-length").value);
-    const inputLength = String(inputElement.value).length;
-    inputElement.classList.add(inputLength < dataLength ? "invalid" : "valid");
+    const inputLength = inputElement.value.length;
+    
+    if (inputLength < dataLength) {
+        inputElement.classList.remove("valid");
+        inputElement.classList.add("invalid");
+    } else {
+        inputElement.classList.remove("invalid");
+        inputElement.classList.add("valid");
+    }    
 };
 
 inputElement.addEventListener("blur", checkInputLength);
