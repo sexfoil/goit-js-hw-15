27c6cc3c928147bd83fe02be843b55e0989f82df
Slava Poliakov <78slavapolyakov@gmail.com>
2023-10-05 21:45:53 +0300

    Solved task 09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const updateColor = () => {
+  const color = getRandomHexColor();
+  document.querySelector("body").style.backgroundColor = color;
+  document.querySelector(".color").textContent = color;
+};
+
+document.querySelector(".change-color").addEventListener("click", updateColor);
