2a357ca6a5b8e833a10754a203887ae5aa5461ee
Slava Poliakov <78slavapolyakov@gmail.com>
2023-10-05 21:36:46 +0300

    Solved task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formElement = document.querySelector(".login-form");
 
+const submitForm = event => {
+    event.preventDefault();
+    const emailElement = formElement.elements.email;
+    const passwordElement = formElement.elements.password;
+
+    if (emailElement.value.length === 0 || passwordElement.value.length === 0) {
+        alert("Email or Passwor can not be empty! Please fill these fields.");
+    } else {
+        const formData = {
+            email: emailElement.value,
+            password: passwordElement.value
+        }
+        console.log(formData);
+        formElement.reset();
+    }    
+};
+
+formElement.addEventListener("submit", submitForm);
